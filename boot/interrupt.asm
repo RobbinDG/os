b1cbdf97e9ff2858431cf7f46199d222bc3e3065
Robbin de Groot <robbindegroot@gmail.com>
2026-03-14 13:47:14 +0100

    Prepared context switching for ring switching

--- boot/interrupt.asm
+++ boot/interrupt.asm
@@ -1,31 +1,6 @@
 [extern isr_handler]
 [extern irq_handler]
-
-; Common ISR code
-isr_common_stub:
-    ; 1. Save CPU state
-	pusha ; Pushes edi,esi,ebp,esp,ebx,edx,ecx,eax
-	mov ax, ds ; Lower 16-bits of eax = ds.
-	push eax ; save the data segment descriptor
-	mov ax, 0x10  ; kernel data segment descriptor
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	
-    ; 2. Call C handler
-	call isr_handler
-	
-    ; 3. Restore state
-	pop eax 
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	popa
-	add esp, 8 ; Cleans up the pushed error code and pushed ISR number
-	sti
-	iret ; pops 5 things at once: CS, EIP, EFLAGS, SS, and ESP
+[extern isr_common_stub]
 
 ; Common IRQ code. Identical to ISR code except for the 'call' 
 ; and the 'pop ebx'
--- src/kernel/platform/i386/context_switch.rs
+++ src/kernel/platform/i386/context_switch.rs
@@ -1,9 +1,10 @@
 use core::{
     arch::{asm, naked_asm},
     mem::offset_of,
+    ptr,
 };
 
-use crate::{KERNEL, scheduler_entrypoint};
+use crate::{SCHEDULER, kernel::isr::InterruptHandlerData, scheduler_entrypoint};
 
 /// Stores all information about a process.
 /// repr(C) is necessary because we need to compute a consistent offset.
@@ -11,15 +12,33 @@ use crate::{KERNEL, scheduler_entrypoint};
 pub struct ProcessCtrlBlock {
     stack_base_ptr: usize, // This is a pointer, but kept as usize to prevent issues with Send/Sync.
     stack_ptr: usize,
-    prog_ctr: usize,
+    pub prog_ctr: usize,
+    interrupt_data: *const InterruptHandlerData,
 }
 
 impl ProcessCtrlBlock {
+    /// Creates a blank PCB that does not contain any meaningful information. This is
+    /// needed to initialise a new PCB for the current thread. This should never be a problem
+    /// as the thread should always store all required information during a context switch
+    /// (so this is a lazy data structure).
+    ///
+    /// TODO is this really a good implementation? Can we not do something with None/Option
+    /// optimisatino that does not cause problems for context switches?
+    pub const fn blank() -> Self {
+        Self {
+            stack_base_ptr: 0xabcdef,
+            stack_ptr: 0xabcdef,
+            prog_ctr: 0xabcdef,
+            interrupt_data: ptr::null(),
+        }
+    }
+
     pub unsafe fn current_process() -> Self {
         let mut s = Self {
             stack_base_ptr: 0,
             stack_ptr: 0,
             prog_ctr: 0,
+            interrupt_data: ptr::null(),
         };
         unsafe {
             asm!(
@@ -38,27 +57,44 @@ impl ProcessCtrlBlock {
             stack_base_ptr,
             stack_ptr: stack_base_ptr,
             prog_ctr: entry_point as usize,
+            interrupt_data: ptr::null(),
         }
+    }
+
+    #[inline]
+    pub fn set_entry(&mut self, func_ptr: usize) {
+        self.prog_ctr = func_ptr;
+    }
+
+    #[inline]
+    pub fn set_interrupt(&mut self, data: *const InterruptHandlerData) {
+        self.interrupt_data = data;
     }
 }
 
 /// We have to write naked ASM here, as Rust will not take the changed stack pointer into account.
 /// We use and follow the C/i386 calling convention to update necessary information. Changing the structure of
 /// `ProcessCtrlBlock` will require updating this as well.
+///
+/// Executing a context switch will save the `cur` context and load the `new` context.
+/// Execution continues from the last saved program counter in `new`.
 #[unsafe(naked)]
 pub unsafe extern "C" fn switch_context(
-    /*entry_point: fn(),*/ cur: &ProcessCtrlBlock,
-    new: &ProcessCtrlBlock,
+    cur: *mut ProcessCtrlBlock,
+    new: *const ProcessCtrlBlock,
 ) -> ! {
     naked_asm!(
         "pop edx", // pop off the top of the original stack; the original return address. Assume that edx is safe to use...
         "pop eax", // First argument
         "pop ecx", // Second argument
-        "mov [eax + {o1}], ebp", // Backup base pointer.
-        "mov [eax + {o2}], esp", // Backup stack pointer.
-        "mov [eax + {o3}], edx", // Backup program counter as the original return address.
-        "mov ebp, [ecx + {o1}]", // update stack base pointer.
-        "mov esp, [ecx + {o2}]", // update stack pointer.
+        "pop esi", // Pop interrupt arg 1 (if any, dangerous if none! Should probably fix this.)
+        "mov dword ptr [eax + {o1}], ebp", // Backup base pointer.
+        "mov dword ptr [eax + {o2}], esp", // Backup stack pointer.
+        "mov dword ptr [eax + {o3}], edx", // Backup program counter as the original return address.
+        "mov ebp, dword ptr [ecx + {o1}]", // update stack base pointer.
+        "mov esp, dword ptr [ecx + {o2}]", // update stack pointer.
+        "push edi", // Push back interrupt info for later use
+        "push esi", // ^
         "push [ecx + {o3}]", // Push function pointer, first argument.
         "call {entry}", // Jump to the entry point of the process. No jmp because the call convention would be broken.
         o1 = const offset_of!(ProcessCtrlBlock, stack_base_ptr),
@@ -73,14 +109,11 @@ pub unsafe extern "C" fn enter_thread(func_ptr: usize) -> ! {
     let func: extern "C" fn() = unsafe { core::mem::transmute(func_ptr) };
     func();
 
-    match KERNEL.get() {
-        Ok(kernel) => unsafe { exit_thread(kernel.scheduler.scheduler_process()) },
-        Err(_) => loop {}, // Unrecoverable error
-    }
+    unsafe { exit_thread(SCHEDULER.with(|s| s.scheduler_process() as *const ProcessCtrlBlock)) };
 }
 
 #[unsafe(naked)]
-pub unsafe extern "C" fn exit_thread(scheduler_ctx: &ProcessCtrlBlock) -> ! {
+pub unsafe extern "C" fn exit_thread(scheduler_ctx: *const ProcessCtrlBlock) -> ! {
     naked_asm!(
         "pop edx", // pop off the top of the original stack; the original return address. Assume that edx is safe to use...
         "pop eax", // First argument
@@ -91,4 +124,26 @@ pub unsafe extern "C" fn exit_thread(scheduler_ctx: &ProcessCtrlBlock) -> ! {
         o2 = const offset_of!(ProcessCtrlBlock, stack_ptr),
         scheduler_entry = sym scheduler_entrypoint,
     )
+}
+
+#[unsafe(naked)]
+pub unsafe extern "C" fn enter_user_mode(entry_point: usize) -> ! {
+    naked_asm!(
+        "pop eax", // Pop original return address. `eax` will be overwritten.
+        "pop edx", // Pop argument
+        "mov ax, (4 * 8) | 3",
+        "mov ds, ax",
+        "mov es, ax",
+        "mov fs, ax",
+        "mov gs, ax",
+        // Prepare `iret` stack frame
+        "mov eax, 0x23",
+        "mov ecx, 0x1b",
+        "push eax",
+        "push 0x7ffff",
+        "pushfd",
+        "push ecx",
+        "push edx", // continue execution from input argument entrypoint.
+        "iretd",    // Compiles to `iret`. Putting `iret` here compiles to `iretw`.
+    );
 }
